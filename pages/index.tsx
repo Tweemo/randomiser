c1aa3ae43d83f3ef10326822d2d093c839ef6b21
Tim Liew <timothywrliew@gmail.com>
2022-12-09 09:19:33 +1300

    added chakraui to look somewaht ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import { useState, useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 import Pairs from './pairs'
 import { shuffle, splitArray } from '../utils/utils'
+import { Button, SimpleGrid, VStack } from '@chakra-ui/react'
 
 export default function Home() {
   
-  const staff = [ 'Craig', 'Tim L', 'Tim P', 'Zaid', 'Thomas', 'Mike', 'Jai', 'Andrew', 'Emily', 'Lance', 'Tariq', 'Phil']
+  const staff = [ 'Craig', 'Tim L', 'Tim P', 'Zaid', 'Mike', 'Jai', 'Andrew', 'Emily', 'Lance', 'Tariq', 'Phil']
   const [pairs, setPairs] = useState<any>()
   
   useEffect(() => {
@@ -13,18 +14,18 @@ export default function Home() {
   }, [])
 
   return (
-      <div className={styles.container}>
-        <button className={styles.resetButton} onClick={() => {
+      <VStack bg='bisque' h='100vh'>
+        <Button className={styles.resetButton} size='lg' colorScheme='teal' onClick={() => {
           setPairs(splitArray(shuffle(staff)))
           console.log([pairs])
         }}>
           Mix&apos;em
-        </button>
-        <div className={styles.cardContainer}>
+        </Button>
+        <SimpleGrid columns={3} gap={'4rem'}>
           {pairs?.map((pair: any, index: number) => (
-            <Pairs key={index} pair={pair} />
+              <Pairs key={index} pair={pair} />
             ))}
-        </div>
-      </div>
+        </SimpleGrid>
+      </VStack>
   )
 }
--- pages/pairs.tsx
+++ pages/pairs.tsx
@@ -1,12 +1,10 @@
+import { Box } from '@chakra-ui/react'
 import styles from '../styles/Home.module.css'
 
 export default function Pairs(pair: any) {
-
   return (
-    <div className={styles.pairs}>
-      <h2>
-        {pair.pair[0]} - {pair.pair[1]}
-      </h2>
-    </div>
+    <Box className={styles.pairs} p='4rem' >
+      {pair.pair[0]} - {pair.pair[1]}
+    </Box>
   )
 }
